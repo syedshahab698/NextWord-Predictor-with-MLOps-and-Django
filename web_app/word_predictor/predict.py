f36c27f68e62e13f5cc20dd4112f74476519c15a
Syed Shahab Uddin <55235435+syedshahab698@users.noreply.github.com>
2023-06-26 15:57:25 -0400

    ml model added

--- web_app/word_predictor/predict.py
+++ web_app/word_predictor/predict.py
@@ -3,16 +3,18 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
+import os
+from django.conf import settings
+
+tokenizer = pickle.load(open(os.path.join(settings.BASE_DIR, 'ml/tokenizer.pkl'), 'rb'))
+new_model = tf.keras.models.load_model(os.path.join(settings.BASE_DIR, 'ml','bilstm_nwp'))
+
+
 
 max_sequence_len = 40
 def predict_next_word(input_text):
 
-    next_words = 2
-
-
-    tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
-    new_model = tf.keras.models.load_model('bilstm_nwp')
-
+    next_words = 10
 
     for _ in range(next_words):
         token_list = tokenizer.texts_to_sequences([input_text])[0]
